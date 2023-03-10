a51a31ee763dc7e2df0e38bb30a0fad92c6e8359
enderta <endertanver@gmail.com>
2023-03-10 20:42:52 +0000

    stk

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-	const [availability, setAvailability] = useState([]);
-	useEffect(() => {
-		fetch(`https://starter-kit-uq32.onrender.com/api/availabilities`,
-			{
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxfSwiaWF0IjoxNjc4NDcyNjkzLCJleHAiOjE2Nzg4MzI2OTN9.XFtFA0dVINwaZ1zjd_yoJaxI7af90yR2fmF3UI-QsQc",
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-
-				},
-			})
-			.then((response) => response.json())
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
+	const  setErr = React.useState(false);
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+	};
+	const handlePassword = (e) => {
+		setPassword(e.target.value);
+	};
+	const handleBack = (e) => {
+		e.preventDefault();
+		window.location.href = "/";
+	};
+	const login = (e) => {
+		e.preventDefault();
+		const user = {
+			email,
+			password,
+		};
+		fetch("/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(user),
+		}).then((res) => res.json())
 			.then((data) => {
+				if (data.token) {
 
-				setAvailability(data.data);
-			});
-	}, [  ]);
-	console.log(availability);
+					localStorage.setItem("token", data.token);
+					localStorage.setItem("email", email);
+					localStorage.setItem("id", data.id);
+					window.location.href = "/availability";
+				} else {
+					alert("Wrong email or password");
+					setErr(true);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	console.log(localStorage);
 
 	return (
         <div>
