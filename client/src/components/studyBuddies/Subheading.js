a92e8da3bde09951fbed632fd957d62006595f8d
enderta <endertanver@gmail.com>
2023-03-12 18:03:40 +0000

    stk

---
+++ client/src/components/studyBuddies/Subheading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Subheading() {
+    return (
+        <div>
+            <h4>Boost your productivity</h4>
+        </div>
+    )
+}
+
+export default Subheading;
